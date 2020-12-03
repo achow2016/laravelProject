13f8fd9f8327c9303e8fb32904d10d383f8b30d1
alan <tilldusk@gmail.com>
2020-12-02 20:11:47 -0800

    fix ajaxSetup error
    change jquery script source on all applicable views to use non-slim version so they can use ajax

--- resources/views/rpgGameStore.blade.php
+++ resources/views/rpgGameStore.blade.php
@@ -60,10 +60,13 @@
 			<button id="returnFromStore" type="button" class="introButtons btn btn-primary active w-100">Home</button>
 		</div>	
 		<!--scripts-->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+		<!--script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
 		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
 		crossorigin="anonymous">
-		</script>
+		</script-->
+		<script src="https://code.jquery.com/jquery-3.5.1.js"
+		integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+		crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
 		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
 		crossorigin="anonymous">
